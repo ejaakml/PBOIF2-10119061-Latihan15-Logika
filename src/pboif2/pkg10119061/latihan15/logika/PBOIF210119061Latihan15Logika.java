0058d55e6240382558ffd5b0866352933a0bf3d5
ejaakml <eximindotong@yahoo.com>
2020-10-20 17:59:22 +0700

    1

---
+++ src/pboif2/pkg10119061/latihan15/logika/PBOIF210119061Latihan15Logika.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.latihan15.logika;
+
+/**
+ *
+ * @author Rheiza
+ */
+public class PBOIF210119061Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       boolean a = true ;
+       boolean b = false;
+       System.out.println("a && b = " + (a && b));
+       System.out.println("a || b = " + (a || b));
+       System.out.println("!a= " + (!a));
+       System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
